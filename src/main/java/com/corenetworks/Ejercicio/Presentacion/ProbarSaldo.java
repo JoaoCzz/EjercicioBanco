6fac4825e19f90054ba7cb53076b25a78ed07f7e
Joao <joaoczz0205@gmail.com>
2024-01-14 22:44:12 +0100

    Initial commit

---
+++ src/main/java/com/corenetworks/Ejercicio/Presentacion/ProbarSaldo.java
@@ -0,0 +1,36 @@
+package com.corenetworks.Ejercicio.Presentacion;
+
+import com.corenetworks.Ejercicio.Persistencia.AccesoCuentaBancaria;
+
+import java.sql.SQLException;
+
+
+public class ProbarSaldo {
+    public static void main(String[] args) {
+        AccesoCuentaBancaria ac= new AccesoCuentaBancaria();
+        try {
+            System.out.println(ac.SolicitarSaldo("12345"));
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+
+        try {
+            System.out.println(ac.Obtenerpin("12345"));
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());        }
+        try {
+            ac.ActualizarSaldo("12345",300);
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+
+    }
+
+
+}
---
+++ src/main/java/com/corenetworks/Presentacion.java
@@ -0,0 +1,4 @@
+package com.corenetworks;
+
+public class Presentacion {
+}
